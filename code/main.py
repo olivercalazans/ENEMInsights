e96690daa03812a43f8a865d8e54479ae9776578
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-02-20 16:58:06 -0300

    FEAT: método para classificar notas

--- code/main.py
+++ code/main.py
@@ -12,11 +12,12 @@ class Main:
     FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
     def __init__(self):
-        self._dicionario        = self._ler_dicionario(self.FILE_PATH)
-        self._maiores_notas     = dict()
-        self._menores_notas     = dict()
-        self._melhores_redacoes = dict()
-        self._dados_gerais      = {
+        self._dicionario         = self._ler_dicionario(self.FILE_PATH)
+        self._contador           = 0
+        self._dados_participante = list()
+        self._maiores_notas      = list()
+        self._menores_notas      = list()
+        self._dados_gerais       = {
             'faixa_etaria':     {chave: 0 for chave in self._dicionario["TP_FAIXA_ETARIA"]},
             'sexo':             {chave: 0 for chave in self._dicionario["TP_SEXO"]},
             'estado_civil':     {chave: 0 for chave in self._dicionario["TP_ESTADO_CIVIL"]},
@@ -40,34 +41,54 @@ class Main:
             FILE_PATH = os.path.join(self.FILE_PATH, 'dados_enem_23.csv')
             with open(FILE_PATH, mode="r", encoding="utf-8") as file:
                 reader = csv.reader(file)
-                contador = 0
                 next(reader)
                 for linha in reader:
-                    dados = linha[0].split(';')
-                    self._processar_dados(dados)
-                    contador += 1
-                    sys.stdout.write(f'\rLinhas processadas: {contador}')
+                    self._dados_participante = linha[0].split(';')
+                    self._processar_dados()
+                    sys.stdout.write(f'\rLinhas processadas: {self._contador}')
                     sys.stdout.flush()
             print('\n')
             self._mostrar_dados()
-        except FileNotFoundError: print('Arquivo de dados não enontrado')
+        except FileNotFoundError: print('Arquivo de dados não encontrado')
         except MemoryError:       print('Memória insuficente para carregar os dados')
+        except Exception as erro: print(f'Erro desconhecido:\n{erro}')
 
 
-    def _processar_dados(self, dados:list) -> None:
-        dados_gerais = [dados[0], dados[1], dados[2], dados[3], dados[4], dados[5], dados[6], dados[11]]
-        self._atualize_dados_gerais(dados_gerais)
+    def _processar_dados(self) -> None:
+        self._atualize_dados_gerais()
+        self._processe_se_houver_todas_as_notas()
+        self._contador += 1
 
 
-    def _atualize_dados_gerais(self, dados_gerais:list) -> None:
+    def _atualize_dados_gerais(self) -> None:
+        dados_gerais = [self._dados_participante[i] for i in [0, 1, 2, 3, 4, 5, 6, 11]]
         for tipo, valor in zip(self._dados_gerais, dados_gerais):
             self._dados_gerais[tipo][valor] += 1
+
+
+    def _processe_se_houver_todas_as_notas(self) -> None:
+        try:    notas = [float(self._dados_participante[i]) for i in [7, 8, 9, 10, 17]]
+        except: pass
+        else:   self._classifique_a_nota(notas)
+
+
+    def _classifique_a_nota(self, notas:list) -> None:
+        nota_geral = round(sum(notas) / 5, 1)
+        self._dados_participante.append(nota_geral)
+        if   nota_geral >= 800: self._maiores_notas.append(self._dados_participante)
+        elif nota_geral <= 300: self._menores_notas.append(self._dados_participante)
 
 
     def _mostrar_dados(self) -> None:
         for dados in self._dados_gerais:
             for i in self._dados_gerais[dados]:
                 print(f'{i} >> {self._dados_gerais[dados][i]}')
+
+        print(len(self._maiores_notas))
+        print(len(self._menores_notas))
+
+
+
 
 
 if __name__ == '__main__':
