c9d1f74e53414366ec1818608babb9f83760933a
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-02-24 16:57:52 -0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -6,19 +6,20 @@
 
 import json, os, csv, sys
 
-
 class Main:
 
-    FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+    FILE_PATH:str       = os.path.dirname(os.path.abspath(__file__))
+    CHAVES_TABELA_GERAL = ["TP_FAIXA_ETARIA", "TP_SEXO", "TP_ESTADO_CIVIL", "TP_COR_RACA", "TP_ST_CONCLUSAO", "TP_ANO_CONCLUIU", "TP_ESCOLA", "TP_STATUS_REDACAO"]
 
     def __init__(self):
-        self._dicionario         = self._ler_dicionario(self.FILE_PATH)
-        self._dados_gerais       = self._criar_dicionario()
-        self._dados_melhores     = self._dados_gerais
-        self._contador           = 0
-        self._dados_participante = list()
-        self._maiores_notas      = list()
-        self._menores_notas      = list()
+        self._dicionario:dict         = self._ler_dicionario(self.FILE_PATH)
+        self._dados_gerais:dict       = self._criar_dicionario(self.CHAVES_TABELA_GERAL)
+        self._dados_melhores:dict     = self._criar_dicionario()
+        self._dados_piores:dict       = self._criar_dicionario()
+        self._dados_participante:list = list()
+        self._contador:int            = 0
+        self._maiores_notas:int       = 0
+        self._menores_notas:int       = 0
 
 
     @staticmethod
@@ -28,10 +29,9 @@ class Main:
             return json.load(file)
 
 
-    def _criar_dicionario(self) -> dict:
-        chaves_dict   = ['faixa_etaria',    'sexo',    'estado_civil',    'cor_raca',    'conclusao_escola', 'ano_conclusao',   'tipo_escola', 'status_redacao']
-        chaves_tabela = ["TP_FAIXA_ETARIA", "TP_SEXO", "TP_ESTADO_CIVIL", "TP_COR_RACA", "TP_ST_CONCLUSAO",  "TP_ANO_CONCLUIU", "TP_ESCOLA",   "TP_STATUS_REDACAO"]
-        return {x: {chave: 0 for chave in self._dicionario[z]} for x, z in zip(chaves_dict, chaves_tabela)}
+    def _criar_dicionario(self, CHAVES=None) -> dict:
+        if not CHAVES: CHAVES = [chave for chave in self._dicionario if chave != 'TP_STATUS_REDACAO']
+        return {x: {chave: 0 for chave in self._dicionario[x]} for x in CHAVES}
 
 
     def _execute(self) -> None:
@@ -53,15 +53,15 @@ class Main:
 
 
     def _processar_dados(self) -> None:
-        self._atualize_dados_gerais()
+        self._atualize_valores(self._dados_gerais, [0, 1, 2, 3, 4, 5, 6, 11])
         self._processe_se_houver_todas_as_notas()
         self._contador += 1
 
 
-    def _atualize_dados_gerais(self) -> None:
-        dados_gerais = [self._dados_participante[i] for i in [0, 1, 2, 3, 4, 5, 6, 11]]
-        for tipo, valor in zip(self._dados_gerais, dados_gerais):
-            self._dados_gerais[tipo][valor] += 1
+    def _atualize_valores(self, dicionario:dict, indices:list) -> None:
+        dados = [self._dados_participante[i] for i in indices]
+        for tipo, valor in zip(dicionario, dados):
+           dicionario[tipo][valor] += 1
 
 
     def _processe_se_houver_todas_as_notas(self) -> None:
@@ -71,9 +71,13 @@ class Main:
 
 
     def _classifique_a_nota(self, notas:list) -> None:
-        nota_geral = round(sum(notas) / 5, 1)
-        if   nota_geral >= 800: self._maiores_notas.append(self._dados_participante)
-        elif nota_geral <= 300: self._menores_notas.append(self._dados_participante)
+        nota_geral = round(sum(notas)/5, 1)
+        if nota_geral >= 800:
+            self._maiores_notas += 1
+            self._atualize_valores(self._dados_melhores, [0, 1, 2, 3, 4, 5, 6, -7, -6, -5, -4, -3, -2, -1])
+        elif nota_geral <= 400:
+            self._menores_notas += 1
+            self._atualize_valores(self._dados_piores, [0, 1, 2, 3, 4, 5, 6, -7, -6, -5, -4, -3, -2, -1])
 
 
     def _mostrar_dados(self) -> None:
@@ -81,8 +85,8 @@ class Main:
             for i in self._dados_gerais[dados]:
                 print(f'{i} >> {self._dados_gerais[dados][i]}')
 
-        print(len(self._maiores_notas))
-        print(len(self._menores_notas))
+        print(self._maiores_notas)
+        print(self._menores_notas)
 
 
 
