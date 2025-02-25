7af7a4506ca5393cd2eecd6ae3ef9e1563b44476
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-02-25 15:06:18 -0300

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -3,7 +3,10 @@
 # Repository: https://github.com/olivercalazans/ENEMInsights
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software...
 
-import json, os, csv, sys, time
+
+import json, os, csv, sys, tkinter
+import tkinter.filedialog
+
 
 # 0 TP_FAIXA_ETARIA         # 10 NU_NOTA_MT
 # 1 TP_SEXO                 # 11 NU_NOTA_REDACAO
@@ -18,18 +21,14 @@ import json, os, csv, sys, time
 
 class Main:
 
-    FILE_PATH:str       = os.path.dirname(os.path.abspath(__file__))
-    CHAVES_TABELA_GERAL = ["TP_FAIXA_ETARIA", "TP_SEXO", "TP_ESTADO_CIVIL", "TP_COR_RACA", "TP_ST_CONCLUSAO", "TP_ANO_CONCLUIU", "TP_ESCOLA"]
-
     def __init__(self):
-        self._dicionario:dict     = self._ler_dicionario(self.FILE_PATH)
-        self._dados_gerais:dict   = self._criar_dicionario(self.CHAVES_TABELA_GERAL)
-        self._dados_melhores:dict = self._criar_dicionario()
-        self._dados_piores:dict   = self._criar_dicionario()
-        self._dados_participante  = list()
-        self._contador:int        = 0
-        self._maiores_notas:int   = 0
-        self._menores_notas:int   = 0
+        self._dicionario:dict         = self._ler_dicionario(os.path.dirname(os.path.abspath(__file__)))
+        self._dados_melhores:dict     = self._criar_dicionario()
+        self._dados_piores:dict       = self._criar_dicionario()
+        self._dados_participante      = list()
+        self._contador:int            = 0
+        self._numero_notas_altas:int  = 0
+        self._numero_notas_baixas:int = 0
 
 
     @staticmethod
@@ -46,32 +45,35 @@ class Main:
 
     def _execute(self) -> None:
         try:
-            self._comece_processamento()
-            self._crie_csv('dados_gerais.csv', self._dados_gerais)
-            self._crie_csv('dados_melhores.csv', self._dados_melhores)
-            self._crie_csv('dados_piores.csv', self._dados_piores)
+            self._processe_base_de_dados()
+            self._crie_arquivos('dados_melhores', self._numero_notas_altas, self._dados_melhores)
+            self._crie_arquivos('dados_piores', self._numero_notas_baixas, self._dados_piores)
         except FileNotFoundError: print('Arquivo de dados não encontrado')
         except MemoryError:       print('Memória insuficente para carregar os dados')
         except Exception as erro: print(f'\nErro desconhecido:\n{erro}')
 
-    
-    def _comece_processamento(self) -> None:
-        FILE_PATH = os.path.join(self.FILE_PATH, 'dados_enem_23.csv')
-        with open(FILE_PATH, mode="r", encoding="utf-8") as file:
-            reader = csv.reader(file)
-            next(reader)
-            for linha in reader:
-                self._dados_participante = linha[0].split(';')
-                self._processar_dados()
+
+    def _processe_base_de_dados(self) -> None:
+        COLUNAS_DESEJADAS  = 'TP_FAIXA_ETARIA;TP_SEXO;TP_ESTADO_CIVIL;TP_COR_RACA;TP_ST_CONCLUSAO;TP_ANO_CONCLUIU;TP_ESCOLA;NU_NOTA_CN;NU_NOTA_CH;NU_NOTA_LC;NU_NOTA_MT;NU_NOTA_REDACAO;Q001;Q002;Q006;Q019;Q022;Q024;Q025'
+        COLUNAS_DESEJADAS  = COLUNAS_DESEJADAS.split(';')
+        ARQUIVO_DE_ENTRADA = self._escolher_arquivo()
+
+        with open(ARQUIVO_DE_ENTRADA, mode='r', encoding='ISO-8859-1') as ARQUIVO_DE_ENTRADA:
+            leitor = csv.DictReader(ARQUIVO_DE_ENTRADA, delimiter=';')
+
+            for linha in leitor:
+                self._dados_participante = [linha[coluna] for coluna in COLUNAS_DESEJADAS if coluna in linha]
+                self._processe_se_houver_todas_as_notas()
+                self._contador += 1
                 sys.stdout.write(f'\rLinhas processadas: {self._contador}')
                 sys.stdout.flush()
-        print('\n')
 
 
-    def _processar_dados(self) -> None:
-        self._atualize_valores(self._dados_gerais, [0, 1, 2, 3, 4, 5, 6])
-        self._processe_se_houver_todas_as_notas()
-        self._contador += 1
+    @staticmethod
+    def _escolher_arquivo() -> str:
+        root = tkinter.Tk()
+        root.withdraw()
+        return tkinter.filedialog.askopenfilename(title="Selecione um arquivo")
 
 
     def _atualize_valores(self, dicionario:dict, indices:list) -> None:
@@ -90,22 +92,29 @@ class Main:
         INDICES    = [0, 1, 2, 3, 4, 5, 6, -7, -6, -5, -4, -3, -2, -1]
         nota_geral = round(sum(notas)/5, 1)
         if nota_geral >= 800:
-            self._maiores_notas += 1
+            self._numero_notas_altas += 1
             self._atualize_valores(self._dados_melhores, INDICES)
         elif nota_geral <= 400:
-            self._menores_notas += 1
+            self._numero_notas_baixas += 1
             self._atualize_valores(self._dados_piores, INDICES)
 
 
-    def _crie_csv(self, nome_do_arquivo:str, dicionario:dict) -> None:
-        with open(nome_do_arquivo, mode='w', newline='', encoding='utf-8') as arquivo:
+    def _crie_arquivos(self, nome_do_arquivo:str, numero_participantes:int, dicionario:dict) -> None:
+        # ARQUIVO CSV
+        with open(nome_do_arquivo + '.csv', mode='w', newline='', encoding='utf-8') as arquivo:
             escritor = csv.writer(arquivo, delimiter=';')
+            escritor.writerow(['total', 'total de participantes', numero_participantes])
             for classe in dicionario:
                 for chave in dicionario[classe]:
                     descricao = self._dicionario[classe][chave]
                     escritor.writerow([classe, descricao, dicionario[classe][chave]])
-        print(f'Arquivo {nome_do_arquivo} criado')
-        
+
+        # ARQUIVO JSON
+        with open(nome_do_arquivo + '.json', 'w') as arquivo:
+            json.dump(dicionario, arquivo, indent=4)
+
+        print(f'Arquivos {nome_do_arquivo}.csv e {nome_do_arquivo}.json criados')
+
 
 
 
